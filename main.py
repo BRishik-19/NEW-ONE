b522ccbeb9d60c29fe02d247fc3afaead7b77bfb
Rishik <rishikbokka78414@gmail.com>
2024-06-08 17:09:01 +0530

    edited text file name

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ dir(st)
 def update():
     new_todo=st.session_state["todoss"]+"\n"
 
-    new= Functions.get_todos(r"webapps\todos_items.txt")
+    new= Functions.get_todos("todos_items.txt")
     new.append(new_todo)
 
-    Functions.write_todos(new, r"webapps\todos_items.txt")
+    Functions.write_todos(new, "todos_items.txt")
 
 st.title("My todo app")
 st.subheader("This is my new app")
 st.write("A simple app adding,editing,completing todos items...")
-todos= Functions.get_todos(r"webapps\todos_items.txt")
+todos= Functions.get_todos("todos_items.txt")
 
 for ind,i in enumerate(todos):
     checkbox=st.checkbox(i,key=i)
     if checkbox:
-        todo= Functions.get_todos(r"webapps\todos_items.txt")
+        todo= Functions.get_todos("todos_items.txt")
         todo.pop(ind)
-        Functions.write_todos(todo, r"webapps\todos_items.txt")
+        Functions.write_todos(todo, "todos_items.txt")
         del st.session_state[i]
         st.experimental_rerun()
 
